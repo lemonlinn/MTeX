db39fbfe0d297eb3074b7a7a739226582b4366ae
Lemon <54917401+lemonlinn@users.noreply.github.com>
2020-03-08 09:53:42 -0400

    Update server.py

--- Backend/server.py
+++ Backend/server.py
@@ -16,14 +16,21 @@ MTeX.get_img("C:/Users/swagj/Documents/GitHub/MTeX/ScienceDirect_Data/", "C:/Use
 data = MTeX.fetch_df(the_path_in = r"C:\Users\swagj\Documents\GitHub\MTeX\Resized Example")
 modelGrid = MTeX.ML_Call(data)
 
-if request.method == "POST":
-    if request.files:
-        image = request.files.get('image', '')
-        MTeXpred = MTeX(image)
-        MTeXpred.prepro(folder = "test_contour")
-        MTeXpred.contour_resize("C:/Users/swagj/Documents/GitHub/MTeX/test", "C:/Users/swagj/Documents/GitHub/MTeX/resize_contour")
-        contour_df = MTeXpred.fetch_contour(r'C:\Users\swagj\Documents\GitHub\MTeX\resize_contour')
-        result = (modelGrid.predict(contour_df))
+def index():
+    if request.method == "POST":
+        if request.files:
+            image = request.files.get('image', '')
+            MTeXpred = MTeX(image)
+            MTeXpred.prepro(folder = "test_contour")
+            MTeXpred.contour_resize("C:/Users/swagj/Documents/GitHub/MTeX/test", "C:/Users/swagj/Documents/GitHub/MTeX/resize_contour")
+            contour_df = MTeXpred.fetch_contour(r'C:\Users\swagj\Documents\GitHub\MTeX\resize_contour')
+            result = (modelGrid.predict(contour_df))
+            
+        return render_template("index.html", info = result)
+    return render_template("index.html", info = result)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug = True)
         
         
         
